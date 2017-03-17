5437267b61099d5b6bde78c63714b0bc9996a794
philipyoo <philipyoo10@gmail.com>
2017-03-17 21:34:47 +0000

    Edit file storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -24,21 +24,19 @@ class Test_FileStorage(unittest.TestCase):
         if os.path.isfile("file.json"):
             self.test_len = len(self.store.all())
 
-#    def tearDown(self):
-#        import os
-#        if os.path.isfile("file.json"):
-#            os.remove('file.json')
-
     def test_all(self):
         self.assertEqual(len(self.store.all()), self.test_len)
 
     def test_new(self):
+        print(len(self.store.all()), self.test_len)
         self.assertEqual(len(self.store.all()), self.test_len)
         self.model.save()
         self.assertEqual(len(self.store.all()), self.test_len + 1)
         a = BaseModel()
         a.save()
         self.assertEqual(len(self.store.all()), self.test_len + 2)
+        self.cli.do_destroy("BaseModel f519fb40-1f5c-458b-945c-2ee8eaaf4900")
+        self.cli.do_destroy("BaseModel " + a.id)
 
     def test_save(self):
         self.test_len = len(self.store.all())
@@ -49,6 +47,8 @@ class Test_FileStorage(unittest.TestCase):
         self.assertNotEqual(len(self.store.all()), self.test_len + 2)
         b.save()
         self.assertEqual(len(self.store.all()), self.test_len + 2)
+        self.cli.do_destroy("BaseModel " + a.id)
+        self.cli.do_destroy("User " + b.id)
 
     def test_reload(self):
         pass
