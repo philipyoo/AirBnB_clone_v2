035194064865b1b156e19c0e9ea4b6763cfa6dd6
philipyoo <philipyoo10@gmail.com>
2017-03-19 02:44:34 +0000

    Edit BaseModel class to include sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-import datetime
+from datetime import datetime
 import uuid
 import models
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """The base class for all storage objects in this project"""
+    id = Column(String, primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
+
     def __init__(self, *args, **kwargs):
         """initialize class object"""
         if len(args) > 0:
             for k in args[0]:
                 setattr(self, k, args[0][k])
         else:
-            self.created_at = datetime.datetime.now()
+            self.created_at = datetime.now()
             self.id = str(uuid.uuid4())
 
     def save(self):
         """method to update self"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
 
@@ -33,4 +41,6 @@ class BaseModel:
         if ("updated_at" in dupe):
             dupe["updated_at"] = str(dupe["updated_at"])
         dupe["__class__"] = type(self).__name__
+        if ("_sa_instance_state" in dupe):
+            dupe.pop("_sa_instance_state", 0)
         return dupe
