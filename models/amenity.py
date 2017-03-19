4ba7f099b841c75196e7db1b927658338a2cc42f
philipyoo <philipyoo10@gmail.com>
2017-03-19 23:58:11 +0000

    Add sqlalchemy to amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
