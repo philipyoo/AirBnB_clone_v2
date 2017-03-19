dab0f2961035380b0e60da140aa9b7bd13c08b0e
philipyoo <philipyoo10@gmail.com>
2017-03-19 00:55:46 +0000

    Handle cleanup for review unittests

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,6 +1,7 @@
 import unittest
 from datetime import datetime
 from models import *
+from console import HBNBCommand
 
 
 class Test_ReviewModel(unittest.TestCase):
@@ -9,8 +10,12 @@ class Test_ReviewModel(unittest.TestCase):
     """
 
     def setUp(self):
+        self.cli = HBNBCommand()
         self.model = Review()
         self.model.save()
+
+    def tearDown(self):
+        self.cli.do_destroy("Review " + self.model.id)
 
     def test_var_initialization(self):
         self.assertTrue(hasattr(self.model, "place_id"))
