42d3687e672d2df0af6991ec03b1de02d601e273
philipyoo <philipyoo10@gmail.com>
2017-03-19 00:59:24 +0000

    Handle cleanup for User unittests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,7 @@
 import unittest
 from datetime import datetime
 from models import *
+from console import HBNBCommand
 
 
 class Test_UserModel(unittest.TestCase):
@@ -9,8 +10,12 @@ class Test_UserModel(unittest.TestCase):
     """
 
     def setUp(self):
+        self.cli = HBNBCommand()
         self.model = User()
         self.model.save()
+
+    def tearDown(self):
+        self.cli.do_destroy("User " + self.model.id)
 
     def test_var_initialization(self):
         self.assertTrue(hasattr(self.model, "email"))
