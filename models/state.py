dea5215a3fdfe6d318bc3ce39c45d9cdbd68495a
philipyoo <philipyoo10@gmail.com>
2017-03-20 02:54:13 +0000

    Add sqlalchemy for state class

--- models/state.py
+++ models/state.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
-class State(BaseModel):
-    name = ""
+class State(BaseModel, Base):
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City",
+                          backref=backref("state",
+                                          cascade="all, delete-orphan"))
 
     def __init__(self, *args, **kwargs):
         super(State, self).__init__(*args, **kwargs)
