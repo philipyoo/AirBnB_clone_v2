5193ac2ba6eaea13cefd2d28e9c7c0983be244af
philipyoo <philipyoo10@gmail.com>
2017-03-20 23:35:37 +0000

    Edit amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 from models import *
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-
-    places = relationship("Place", secondary="place_amenity", viewonly=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
