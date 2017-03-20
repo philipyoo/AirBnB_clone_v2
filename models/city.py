a2059aabfe05a311fdcb1dd43661c2674e9e9635
philipyoo <philipyoo10@gmail.com>
2017-03-20 00:00:37 +0000

    Add sqlalchemy to city class

--- models/city.py
+++ models/city.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class City(BaseModel):
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
