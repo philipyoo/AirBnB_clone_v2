cdf2897876a7c3132ee97c8c7550c571c90c2509
philipyoo <philipyoo10@gmail.com>
2017-03-20 02:17:59 +0000

    Add sqlalchemy for review class

--- models/review.py
+++ models/review.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class Review(BaseModel):
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    __tablename__ = "reviews"
+    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    text = Column(String(1024), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
