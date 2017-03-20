2859c853e4ae9b5d172ee0bd334fe2050126a589
philipyoo <philipyoo10@gmail.com>
2017-03-20 03:03:22 +0000

    Add sqlalchemy for User objects

--- models/user.py
+++ models/user.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
-class User(BaseModel):
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship("Place",
+                          backref=backref("user",
+                                          cascade="all, delete-orphan"))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
