d02f5b2b5dc0a26e87bf8363e4245777e84c0fec
philipyoo <philipyoo10@gmail.com>
2017-03-20 04:16:01 +0000

    Add sqlalchemy for place object

--- models/place.py
+++ models/place.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
-class Place(BaseModel):
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenities = []
+class Place(BaseModel, Base):
+    __tablename__ = "places"
+    city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
+
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=True)
 
     def __init__(self, *args, **kwargs):
-        super().__init__()
+        super().__init__(*args, **kwargs)
 
-        if kwargs is not None:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
+
+class PlaceAmenity(Base):
+    __tablename__ = "place_amenity"
+    place_id = Column(String(60), nullable=False, ForeignKey('places.id'),
+                      primary_key=True)
+    amenity_id = Column(String(60), nullable=False, ForeignKey('amenities.id'),
+                        primary_key=True)
+
+    place = relationship("Place", backref=backref("place_amenity")
