1d7ee594836fd7bb4cfaecbd9ec38c6db33e8cad
philipyoo <philipyoo10@gmail.com>
2017-03-21 01:16:27 +0000

    Add select priv to hbnb_dev setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,6 +7,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
 IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- User 'hbnb_dev' should have SELECT privileges on db 'performance_schema' --
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
+
 -- User 'hbnb_dev' should have all privileges on db 'hbnb_dev_db' --
 GRANT ALL PRIVILEGES ON hbnb_dev_db.*
 TO 'hbnb_dev'@'localhost';
