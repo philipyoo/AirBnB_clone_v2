1ae228e116ef31be2c109e31114c8bf11761c9e1
philipyoo <philipyoo10@gmail.com>
2017-03-21 00:04:14 +0000

    Edit models/init file to handle both filestorage and mysql store and read of data

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.amenity import Amenity
@@ -7,7 +8,12 @@ from models.place import Place
 from models.state import State
 from models.review import Review
 from models.engine import file_storage
+from models.engine import db_storage
 
 
-storage = file_storage.FileStorage()
+if os.environ['HBNB_TYPE_STORAGE'] == "db":
+    storage = db_storage.DBStorage()
+else:
+    storage = file_storage.FileStorage()
+
 storage.reload()
