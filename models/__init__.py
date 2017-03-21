962531cf41d52ffe363821cb198e1c47c42d43cc
philipyoo <philipyoo10@gmail.com>
2017-03-21 23:42:31 +0000

    Comment back in the if else statement for different storage type in init file

--- models/__init__.py
+++ models/__init__.py
@@ -12,9 +12,8 @@ from models.engine import file_storage
 from models.engine import db_storage
 
 
-# if os.environ['HBNB_TYPE_STORAGE'] == "db":
-#    storage = db_storage.DBStorage()
-# else:
-#    storage = file_storage.FileStorage()
-storage = file_storage.FileStorage()
+if os.environ['HBNB_TYPE_STORAGE'] == "db":
+    storage = db_storage.DBStorage()
+else:
+    storage = file_storage.FileStorage()
 storage.reload()
