f786a8d9cd9544d5d13237e65401fbbe70ea3967
philipyoo <philipyoo10@gmail.com>
2017-03-28 22:56:48 +0000

    Edit flask 4 to comply with pep8

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -3,24 +3,29 @@
 from flask import Flask, abort
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<string:s>')
 def c(s):
     new_s = s.replace("_", " ")
     return "C {}".format(new_s)
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:s>')
 def python(s="is cool"):
     new_s = s.replace("_", " ")
     return "Python {}".format(new_s)
+
 
 @app.route('/number/<n>')
 def number(n):
