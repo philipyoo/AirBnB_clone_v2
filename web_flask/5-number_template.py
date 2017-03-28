17fe483b9e7a48b186085ce5a53a9ccbc169a310
philipyoo <philipyoo10@gmail.com>
2017-03-28 22:58:28 +0000

    Edit flask 5 to comply with pep8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,18 +3,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<string:s>')
 def c(s):
     new_s = s.replace("_", " ")
     return "C {}".format(new_s)
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:s>')
@@ -22,9 +26,11 @@ def python(s="is cool"):
     new_s = s.replace("_", " ")
     return "Python {}".format(new_s)
 
+
 @app.route('/number/<int:n>')
 def number(n):
     return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
