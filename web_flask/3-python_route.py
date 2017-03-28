788db1ef4103dbf2bfd5506e321433d80fee949e
philipyoo <philipyoo10@gmail.com>
2017-03-28 22:56:09 +0000

    Edit flask 3 to comply with pep8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,18 +3,22 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<string:s>')
 def c(s):
     new_s = s.replace("_", " ")
     return "C {}".format(new_s)
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:s>')
