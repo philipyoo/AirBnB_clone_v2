c469558bd5759440aab4b71daa89dd3d05ace4f9
philipyoo <philipyoo10@gmail.com>
2017-03-28 23:01:42 +0000

    Edit flask 7 to comply with pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,18 +4,22 @@ from models import *
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb')
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<string:s>')
 def c(s):
     new_s = s.replace("_", " ")
     return "C {}".format(new_s)
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:s>')
@@ -23,22 +27,27 @@ def python(s="is cool"):
     new_s = s.replace("_", " ")
     return "Python {}".format(new_s)
 
+
 @app.route('/number/<int:n>')
 def number(n):
     return "{} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
     return render_template('5-number.html', num=n)
 
+
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', num=n)
+
 
 @app.route('/states_list')
 def states_list():
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown(err):
